05a70366b4c7d12e1226ed90b10440c21c09e36b
senn59 <74777070+senn59@users.noreply.github.com>
2024-10-03 13:56:20 +0200

    init project with basic structure and communication

---
+++ ProjectAlchemy.Core/Domain/WorkItem.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectAlchemy.Core.Domain;
+
+public class WorkItem
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    // public int Key { get; set; }
+    // public WorkItemType Type { get; init; }
+    public WorkItem(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+    
+    public void SetDescription(string description)
+    {
+        Description = description;
+    }
+}
