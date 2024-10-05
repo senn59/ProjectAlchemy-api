3f7ed8545d7b9113c851481d1f393b77ddfd6d8e
senn59 <74777070+senn59@users.noreply.github.com>
2024-10-06 00:02:28 +0200

    feat: expand issue model

--- ProjectAlchemy.Web/Dtos/UpdateIssueRequest.cs
+++ ProjectAlchemy.Web/Dtos/UpdateIssueRequest.cs
@@ -1,7 +1,10 @@
+using ProjectAlchemy.Core.Domain;
+
 namespace ProjectAlchemy.Web.Dtos;
 
 public class UpdateIssueRequest
 {
     public required string Name { get; set; }
     public required string? Description { get; set; }
+    public required IssueType Type { get; set; }
 }
