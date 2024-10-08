0ff6397e4aa25c5a0d6b3a3c6a69052650ac799e
senn59 <74777070+senn59@users.noreply.github.com>
2024-10-08 13:51:35 +0200

    feat: add basic delete

--- ProjectAlchemy.Web/Controllers/IssueController.cs
+++ ProjectAlchemy.Web/Controllers/IssueController.cs
@@ -6,7 +6,7 @@ using ProjectAlchemy.Web.Dtos;
 namespace ProjectAlchemy.Web.Controllers;
 
 [ApiController]
-[Route("api/Issue")]
+[Route("api/issues")]
 public class IssueController : ControllerBase
 {
     private readonly ILogger<IssueController> _logger;
@@ -18,21 +18,21 @@ public class IssueController : ControllerBase
         _IssueService = IssueService;
     }
 
-    [HttpGet(Name = "Get all work items")]
+    [HttpGet(Name = "Get all issues")]
     public IEnumerable<IssueResponse> Get()
     {
         var items = _IssueService.GetAll();
         return items.Select(i => IssueResponse.FromIssue(i));
     }
 
-    [HttpGet("{id:int}",Name = "Get work item")]
+    [HttpGet("{id:int}",Name = "Get issue")]
     public IssueResponse Get(int id)
     {
         var item =  _IssueService.GetById(id);
         return IssueResponse.FromIssue(item);
     }
 
-    [HttpPost(Name = "Create work item")]
+    [HttpPost(Name = "Create issue")]
     public IssueResponse Post(CreateIssueRequest request)
     {
         var converted = CreateIssueRequest.ToIssue(request);
@@ -40,11 +40,17 @@ public class IssueController : ControllerBase
         return IssueResponse.FromIssue(item);
     }
 
-    [HttpPut("{id:int}", Name = "Update work item")]
+    [HttpPut("{id:int}", Name = "Update issue")]
     public IssueResponse Put(UpdateIssueRequest request, int id)
     {
         var converted = new Issue(id, request.Name, request.Type, request.Description);
         var item = _IssueService.Update(converted);
         return IssueResponse.FromIssue(item);
+    }
+    
+    [HttpDelete("{id:int}", Name = "Delete issue")]
+    public void Delete(int id)
+    {
+        _IssueService.DeleteById(id);
     }
 }
