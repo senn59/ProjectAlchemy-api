6f0ddaa3b55ddc99a669a6781f9cb85d6786e811
senn59 <74777070+senn59@users.noreply.github.com>
2024-11-17 19:40:52 +0100

    chore: autodetect sql version instead of manually entering

--- ProjectAlchemy.Persistence/AppDbContext.cs
+++ ProjectAlchemy.Persistence/AppDbContext.cs
@@ -10,7 +10,7 @@ public class AppDbContext(string connectionString) : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder
-            .UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0)));
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
