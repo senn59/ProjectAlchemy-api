7a63dc3a8436daffc94dbd1d08020a09695f9bf6
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-01 15:27:43 +0100

    feat: implement member repository

--- ProjectAlchemy.Core/Dto/ProjectOverview.cs
+++ ProjectAlchemy.Core/Dto/ProjectOverview.cs
@@ -1,7 +1,10 @@
+using ProjectAlchemy.Core.Domain;
+
 namespace ProjectAlchemy.Core.Dto;
 
 public class ProjectOverview
 {
-    public int Id { get; init; }
-    public string Name { get; init; }
+    public string ProjectId { get; init; }
+    public string ProjectName { get; init; }
+    public MemberType MemberType { get; init; }
 }
