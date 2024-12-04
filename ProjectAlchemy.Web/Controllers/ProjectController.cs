3b6a320976cbc415791c32595ab84433c6bdf100
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-04 17:05:03 +0100

    feat: add create project endpoint

--- ProjectAlchemy.Web/Controllers/ProjectController.cs
+++ ProjectAlchemy.Web/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using Azure.Core;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using ProjectAlchemy.Core.Domain;
@@ -34,11 +35,19 @@ public class ProjectController : ControllerBase
         return projects;
     }
 
-    [HttpGet("{id}",Name = "Get Project")]
+    [HttpGet("{id}")]
     public async Task<ProjectView> Get(string id)
     {
         var userId = JwtHelper.GetId(User);
         var project = await _projectService.Get(id, userId);
+        return ProjectView.FromProject(project);
+    }
+
+    [HttpPost]
+    public async Task<ProjectView> CreateProject(CreateProjectRequest request)
+    {
+        var userId = JwtHelper.GetId(User);
+        var project = await _projectService.Create(request.Name, userId);
         return ProjectView.FromProject(project);
     }
 }
