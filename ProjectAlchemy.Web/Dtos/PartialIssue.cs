5b4c9a058a5f709da3ff941616f79b11c9b41f16
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-05 15:28:57 +0100

    feat: add lane to partial issue dto

--- ProjectAlchemy.Web/Dtos/PartialIssue.cs
+++ ProjectAlchemy.Web/Dtos/PartialIssue.cs
@@ -7,6 +7,7 @@ public class PartialIssue
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required IssueType Type { get; set; }
+    public required string Lane { get; set; }
 
     public static PartialIssue FromIssue(Issue issue)
     {
@@ -14,7 +15,8 @@ public class PartialIssue
         {
             Id = issue.Id,
             Name = issue.Name,
-            Type = issue.Type
+            Type = issue.Type,
+            Lane = issue.Lane.Name
         };
     }
 }
