68d27955e81727a9fd06a2d1c0e3d613d87da8f1
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-12 10:46:08 +0100

    feat: add integration test

--- ProjectAlchemy.CoreTests/ProjectTests.cs
+++ ProjectAlchemy.CoreTests/ProjectTests.cs
@@ -1,24 +1,44 @@
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using ProjectAlchemy.Core.Domain;
 using ProjectAlchemy.Core.Exceptions;
+using ProjectAlchemy.Core.Services;
+using ProjectAlchemy.Persistence;
+using ProjectAlchemy.Persistence.Repositories;
 
 namespace ProjectAlchemy.CoreTests;
 
 public class Tests
 {
-    [Test]
-    public async Task ProjectNameIsValided()
+    private AppDbContext _context;
+    private ProjectService _service;
+    
+    [SetUp]
+    public void Init()
     {
-        var validName = new string('*', Project.MaxNameLength);
-        var tooLongName = new string('*', Project.MaxNameLength + 1);
-        var emptyName = "";
-        Action createProjectWithValidName = () => new Project(validName, [], [], []);
-        Action createProjectWithShortName = () => new Project("1", [], [], []);
-        Action createProjectWithTooLongName = () => new Project(tooLongName, [], [], []);
-        Action createProjectWithEmptyName = () => new Project(emptyName, [], [], []);
-        createProjectWithValidName.Should().NotThrow();
-        createProjectWithShortName.Should().NotThrow();
-        createProjectWithTooLongName.Should().Throw<InvalidArgument>();
-        createProjectWithEmptyName.Should().Throw<InvalidArgument>();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("db")
+            .Options;
+        _context = new AppDbContext(options);
+        var projectRepository = new ProjectRepository(_context);
+        _service = new ProjectService(projectRepository, new AuthorizationService(projectRepository));
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+    
+    [Test]
+    public async Task TryingToAccessProjectWhereUserIsNotAMemberThrowsUnauthorizedException()
+    {
+        var memberOneProject = await _service.Create("test", "1");
+        //ensure a 2nd member exists
+        await _service.Create("test2", "2");
+
+        var action = () => _service.Get(memberOneProject.Id, "2");
+        await action.Should().ThrowAsync<UnauthorizedAccessException>();
     }
 }
