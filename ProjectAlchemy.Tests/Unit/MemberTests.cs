ca2b0bad962c2c2a5bf13ca88f5d695e24ab6099
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-13 16:59:19 +0100

    feat: add member unit tests

--- ProjectAlchemy.Tests/Unit/MemberTests.cs
+++ ProjectAlchemy.Tests/Unit/MemberTests.cs
@@ -1,6 +1,35 @@
+using FluentAssertions;
+using ProjectAlchemy.Core.Domain;
+
 namespace ProjectAlchemy.Tests.Unit;
 
 public class MemberTests
 {
-    
+    [Fact]
+    public void OwnerCanUpdateIssuesReturnsTrue()
+    {
+        var member = new Member("1", MemberType.Owner);
+        member.CanUpdateIssues().Should().BeTrue();
+    }
+
+    [Fact]
+    public void OwnerCanDeleteIssuesReturnsTrue()
+    {
+        var member = new Member("1", MemberType.Owner);
+        member.CanDeleteIssues().Should().BeTrue();
+    }
+
+    [Fact]
+    public void CollaboratorCanUpdateIssuesReturnsTrue()
+    {
+        var member = new Member("1", MemberType.Collaborator);
+        member.CanUpdateIssues().Should().BeTrue();
+    }
+
+    [Fact]
+    public void CollaboratorCanDeleteIssuesReturnsFalse()
+    {
+        var member = new Member("1", MemberType.Collaborator);
+        member.CanUpdateIssues().Should().BeFalse();
+    }
 }
