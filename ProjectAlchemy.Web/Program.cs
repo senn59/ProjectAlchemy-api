a3c292097fdfcf055bd652c7d8bb199140b902b4
senn59 <74777070+senn59@users.noreply.github.com>
2024-12-26 13:13:26 +0100

    fix: add authorization service as interface implementation to DI

--- ProjectAlchemy.Web/Program.cs
+++ ProjectAlchemy.Web/Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddScoped<IIssueRepository, IssueRepository>();
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddScoped<ILaneRepository, LaneRepository>();
-builder.Services.AddScoped<AuthorizationService>();
+builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
 builder.Services.AddScoped<IssueService>();
 builder.Services.AddScoped<ProjectService>();
 builder.Services.AddScoped<UserService>();
